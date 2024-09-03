9cd6fc2588a8894cd93e31c41aa0a8bde587057c
Abhinandan Parhi <127218863+abhinandansgit@users.noreply.github.com>
2024-09-03 21:39:15 +0530

    Create pipgem.py

---
+++ pipgem.py
@@ -0,0 +1,21 @@
+!pip install -q -U google-generativeai
+!pip install gradio google-generativeai
+import os
+import google.generativeai as genai
+%env GOOGLE_API_KEY=<AIzaSyDumCbUhY9MLOcYLbkCn7kiuD5a01Hz6V0> 
+!python gemini_bot.py
+import google.generativeai as genai
+%env GENERATIVE_AI_ENDPOINT=https://generativelanguage.googleapis.com
+
+genai.configure(api_key='**************************')
+   
+for m in genai.list_models():
+      if 'generateContent' in m.supported_generation_methods:
+           print(m.name)
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+prompt = prompt = input("wellcome to Abhinandan's AI chat\n Ask me anything: ")
+response = chat.send_message(prompt, stream=True)
+for chunk in response:
+  if chunk.text:
+    print(chunk.text)
